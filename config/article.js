cd089636f5fb41e902452096dd590d4c4915a9e7
Martin Stabe <martinstabe@gmail.com>
2017-04-21 15:46:39 +0100

    Make date dynamic

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date('2017-04-13T05:30:00Z'),
+  publishedDate: new Date(),
 
   headline: 'Do company diversity programmes actually help women get ahead?',
 
